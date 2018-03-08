c3872ab84215d9db74945d5db28d3559be8a5f8e
saleson <qlichunyu@163.com>
2018-03-08 16:19:00 +0800

    修复issue#2

--- fm-cloud/fm-cloud-plugins/fm-cloud-graybunny/src/main/java/com/fm/gray/core/GrayPolicyGroup.java
+++ fm-cloud/fm-cloud-plugins/fm-cloud-graybunny/src/main/java/com/fm/gray/core/GrayPolicyGroup.java
@@ -6,69 +6,66 @@ import java.util.List;
 
 public class GrayPolicyGroup {
 
-    private String policyGroupId;
-    private String alias;
-    private List<GrayPolicy> list = new ArrayList<>();
-    private boolean enable = true;
+  private String policyGroupId;
+  private String alias;
+  private List<GrayPolicy> list = new ArrayList<>();
+  private boolean enable = true;
 
-    public String getPolicyGroupId() {
-        return policyGroupId;
+  public String getPolicyGroupId() {
+    return policyGroupId;
+  }
+
+  public void setPolicyGroupId(String policyGroupId) {
+    this.policyGroupId = policyGroupId;
+  }
+
+  public void addGrayPolicy(GrayPolicy policy) {
+    removeGrayPolicy(policy.getPolicyId());
+    list.add(policy);
+  }
+
+  public String getAlias() {
+    return alias;
+  }
+
+  public void setAlias(String alias) {
+    this.alias = alias;
+  }
+
+  public List<GrayPolicy> getList() {
+    return list;
+  }
+
+  public void setList(List<GrayPolicy> list) {
+    this.list = list;
+  }
+
+  public GrayPolicy removeGrayPolicy(String policyId) {
+    Iterator<GrayPolicy> iter = list.iterator();
+    while (iter.hasNext()) {
+      GrayPolicy policy = iter.next();
+      if (policy.getPolicyId().equals(policyId)) {
+        iter.remove();
+        return policy;
+      }
     }
+    return null;
+  }
 
-    public void setPolicyGroupId(String policyGroupId) {
-        this.policyGroupId = policyGroupId;
+  public GrayPolicy getGrayPolicy(String policyId) {
+    for (GrayPolicy grayPolicy : list) {
+      if (grayPolicy.getPolicyId().equals(policyId)) {
+        return grayPolicy;
+      }
     }
+    return null;
+  }
 
-    public void addGrayPolicy(GrayPolicy policy){
-        removeGrayPolicy(policy.getPolicyId());
-        list.add(policy);
-    }
+  public boolean isEnable() {
+    return enable;
+  }
 
-
-    public String getAlias() {
-        return alias;
-    }
-
-    public void setAlias(String alias) {
-        this.alias = alias;
-    }
-
-    public List<GrayPolicy> getList() {
-        return list;
-    }
-
-    public void setList(List<GrayPolicy> list) {
-        this.list = list;
-    }
-
-    public GrayPolicy removeGrayPolicy(String policyId) {
-        Iterator<GrayPolicy> iter= list.iterator();
-        while (iter.hasNext()){
-            GrayPolicy policy = iter.next();
-            if(policy.getPolicyId().equals(policyId)){
-                iter.remove();
-                return policy;
-            }
-        }
-        return null;
-    }
-
-
-    public GrayPolicy getGrayPolicy(String policyId){
-        for (GrayPolicy grayPolicy : list) {
-            if(grayPolicy.getPolicyId().equals(policyGroupId)){
-                return grayPolicy;
-            }
-        }
-        return null;
-    }
-
-
-    public boolean isEnable() {
-        return enable;
-    }
-
-    public void setEnable(boolean enable) {
-        this.enable = enable;
-    }
+  public void setEnable(boolean enable) {
+    this.enable = enable;
+  }
 }
